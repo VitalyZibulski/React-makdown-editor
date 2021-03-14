ffd20b86e36bfacea62143bcf272de1b8f25a65b
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2021-03-14 21:45:13 +0300

    add react-markdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import marked from 'marked';
+import ReactMarkdown from "react-markdown";
 import './App.css';
 
 export default function App() {
@@ -12,11 +13,7 @@ export default function App() {
   return (
     <div className="app">
       <textarea onChange={handleChange} value={markdown} />
-
-      <div
-        className="preview"
-        dangerouslySetInnerHTML={{ __html: marked(markdown)}}
-      />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
